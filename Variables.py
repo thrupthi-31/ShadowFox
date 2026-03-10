d6f812f90658cf289d6dd4bb511010196241e3b0
thrupthi-31 <poojarythrupthi31@gmail.com>
2026-03-10 14:56:35 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,9 @@
+pi = 22/7
+print("Value of pi:", pi)
+print("Datatype of pi:", type(pi))
+print("\nCannot create variable named 'for' because it is a reserved keyword in Python.")
+P = 1000  
+R = 5      
+T = 3      
+SI = (P * R * T) / 100
+print("\nSimple Interest for 3 years:", SI)
